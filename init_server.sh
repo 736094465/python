320f35c4c2fe9e113551e78fe8add71f2cf90bf1
736094465 <736094465@qq.com>
2018-12-06 23:49:34 +0800

    init

--- init_server.sh
+++ init_server.sh
@@ -10,3 +10,5 @@ apt-get install mysql-server
 mkdir project
 git clone  https://github.com/736094465/python.git ./project/
 cd project/
+git config --global user.email "736094465@qq.com"
+git config --global user.name "736094465"
